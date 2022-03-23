fed2d84355df23805f8b23dceafce03126ba506c
maxgfr <25312957+maxgfr@users.noreply.github.com>
2022-03-23 18:16:21 +0100

    fix: arch

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import './controllers';
 import bodyParser from 'body-parser';
 import { Container } from 'inversify';
 import { InversifyExpressServer } from 'inversify-express-utils';
+import type { NextFunction, Request, Response } from "express";
 
 import { IndexService } from './services';
 import { getName } from './utils';
@@ -21,6 +22,12 @@ server.setConfig((srv) => {
     }),
   );
   srv.use(bodyParser.json());
+});
+
+server.setErrorConfig((app) => {
+  app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
+    res.status(500).json({ error: err.message });
+  });
 });
 
 const app = server.build();
